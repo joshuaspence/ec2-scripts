8f2fb321d01fb9fdd82ae0a39c46eaec986ca1c5
Joshua Spence <josh@freelancer.com>
2018-07-09 09:02:10 +1000

    Double spacing

--- lib/ec2-ssh.sh
+++ lib/ec2-ssh.sh
@@ -53,7 +53,7 @@ readonly HOSTNAME=$(echo "${INSTANCE}" | jq --raw-output --exit-status 'if .Publ
 # TODO: This would be easier if `aws elb describe-load-balancers` provided a
 # `--filters` flag, similar to `aws ec2 describe-instances`.
 readonly VPC_ID=$(echo "${INSTANCE}" | jq --raw-output --exit-status '.VpcId')
-readonly JUMPHOST_SG=$(aws "${AWS_OPTS[@]}" --output text ec2 describe-security-groups --filters  'Name=tag:Name,Values=jumphost_lb' 'Name=tag:role,Values=jumphost' "Name=vpc-id,Values=${VPC_ID}" --query 'SecurityGroups[0].GroupId')
+readonly JUMPHOST_SG=$(aws "${AWS_OPTS[@]}" --output text ec2 describe-security-groups --filters 'Name=tag:Name,Values=jumphost_lb' 'Name=tag:role,Values=jumphost' "Name=vpc-id,Values=${VPC_ID}" --query 'SecurityGroups[0].GroupId')
 readonly JUMPHOST=$(aws "${AWS_OPTS[@]}" --output json elb describe-load-balancers | jq --arg jumphost_sg "${JUMPHOST_SG}" --raw-output --exit-status '.LoadBalancerDescriptions[] | select(.SecurityGroups[] | contains($jumphost_sg)) | .DNSName')
 if [[ -n $JUMPHOST ]]; then
   SSH_OPTS+=("-o ProxyCommand=ssh ${SSH_OPTS[*]} -W %h:%p ${JUMPHOST}")
