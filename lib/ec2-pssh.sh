ca2008630a28332fccef6177621e35c65d3bca04
Joshua Spence <josh@freelancer.com>
2018-07-09 09:59:37 +1000

    Add `ec2-pssh` script

---
+++ lib/ec2-pssh.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+AWS_OPTS=()
+FILTERS=()
+PSSH_OPTS=()
+SSH_OPTS=()
+
+# Read command-line flags.
+eval set -- "$(getopt --longoptions aws-profile:,aws-region:,environment:,product:,profile:,role:,stack: --name "$0" --options '' -- "$@")"
+
+while true; do
+  case $1 in
+    --aws-*)
+      AWS_OPTS+=("--${1#--aws-}=${2}")
+      shift 2
+      ;;
+
+    # Just assume that any other provided flag, which isn't prefixed with
+    # `aws-`, is a tag filter.
+    --?*)
+      FILTERS+=("Name=tag:${1#--},Values=${2}")
+      shift 2
+      ;;
+
+    --)
+      shift
+      break
+      ;;
+
+    *)
+      echo "Not implemented: $1" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [[ ${#AWS_OPTS[@]} == 0 ]]; then
+  echo 'No AWS options were specified.' >&2
+  exit 1
+fi
+
+if [[ $# == 0 ]]; then
+  echo 'No command was specified.' >&2
+  exit 1
+fi
+
+# Only include running instances.
+FILTERS+=('Name=instance-state-name,Values=running')
+
+# Query the specified instances.
+readonly INSTANCES=$(aws "${AWS_OPTS[@]}" --output json ec2 describe-instances --filters "${FILTERS[@]}" --query 'Reservations[*].Instances[0]')
+readonly VPC_ID=$(echo "${INSTANCES}" | jq --raw-output --exit-status '.[].VpcId' | sort | uniq)
+
+# Ensure that all instances are in the same VPC.
+if [[ $(echo "${VPC_ID}" | wc --lines) -gt 1 ]]; then
+  echo 'Instances must all reside in the same VPC.' >&2
+  exit 1
+fi
+
+PSSH_OPTS+=('--timeout=0')
+
+PSSH_OPTS+=('--option=LogLevel=ERROR')
+SSH_OPTS+=('-o LogLevel=ERROR')
+PSSH_OPTS+=('--option=StrictHostKeyChecking=no')
+SSH_OPTS+=('-o StrictHostKeyChecking=no')
+PSSH_OPTS+=('--option=UserKnownHostsFile=/dev/null')
+SSH_OPTS+=('-o UserKnownHostsFile=/dev/null')
+
+# Find the jumphost
+# Discover the VPC jumphost.
+#
+# TODO: This would be easier if `aws elb describe-load-balancers` provided a
+# `--filters` flag, similar to `aws ec2 describe-instances`.
+readonly JUMPHOST_SG=$(aws "${AWS_OPTS[@]}" --output text ec2 describe-security-groups --filters 'Name=tag:Name,Values=jumphost_lb' 'Name=tag:role,Values=jumphost' "Name=vpc-id,Values=${VPC_ID}" --query 'SecurityGroups[0].GroupId')
+readonly JUMPHOST=$(aws "${AWS_OPTS[@]}" --output json elb describe-load-balancers | jq --arg jumphost_sg "${JUMPHOST_SG}" --raw-output --exit-status '.LoadBalancerDescriptions[] | select(.SecurityGroups[] | contains($jumphost_sg)) | .DNSName')
+if [[ -n $JUMPHOST ]]; then
+  PSSH_OPTS+=("--option=ProxyCommand=ssh ${SSH_OPTS[*]} -W %h:%p ${JUMPHOST}")
+fi
+
+# shellcheck disable=SC2207
+HOSTS=($(echo "${INSTANCES}" | jq --raw-output --exit-status '.[] | "--host=" + if .PublicDnsName != "" then .PublicDnsName else .PrivateDnsName end'))
+
+set -x
+pssh \
+  "${HOSTS[@]}" \
+  --timeout=0 \
+  "${PSSH_OPTS[@]}" \
+  --verbose \
+  --inline \
+  -- \
+  "$@"
