19ce293966967bf25ec0d31fb0b91e3230af5419
Joshua Spence <josh@freelancer.com>
2018-07-09 11:55:24 +1000

    Minor tidying

--- lib/ec2-pssh.sh
+++ lib/ec2-pssh.sh
@@ -35,29 +35,28 @@ fi
 PSSH_OPTS=()
 PSSH_OPTS+=('--timeout=0')
 
-PSSH_OPTS+=('--option=LogLevel=ERROR')
-PSSH_OPTS+=('--option=StrictHostKeyChecking=no')
-PSSH_OPTS+=('--option=UserKnownHostsFile=/dev/null')
+# TODO: This seems rather hacky.
+for SSH_OPT in "${SSH_OPTS[@]}"; do
+  PSSH_OPTS+=("--option=${SSH_OPT#-o }")
+done
 
 readonly JUMPHOST=$(find_jumphost "${VPC_ID[0]}")
 if [[ -n $JUMPHOST ]]; then
   PSSH_OPTS+=("--option=ProxyCommand=ssh ${SSH_OPTS[*]} -W %h:%p ${JUMPHOST}")
 fi
 
-# shellcheck disable=SC2207
-HOSTS=($(
+mapfile -t HOSTS < <(
   echo "${INSTANCES}" \
   | jq \
     --raw-output --exit-status \
     '.[] | "--host=" + if .PublicDnsName != "" then .PublicDnsName else .PrivateDnsName end'
-))
+)
 
 set -x
 pssh \
   "${HOSTS[@]}" \
   --timeout=0 \
   "${PSSH_OPTS[@]}" \
-  --verbose \
   --inline \
   -- \
   "$@"
