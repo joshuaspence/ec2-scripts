c0cb823c08e68b95e433583a7bf07a6182320543
Joshua Spence <josh@freelancer.com>
2018-07-09 13:54:59 +1000

    Add `ec2-rsync` script

---
+++ lib/ec2-rsync.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# shellcheck source=lib/common.sh
+source "$(dirname "$(readlink --canonicalize "${BASH_SOURCE[0]}")")/common.sh"
+
+# TODO: This seems rather hacky.
+for ii in "${!AWS_OPTS[@]}"; do
+  AWS_OPTS[$ii]="--aws-${AWS_OPTS[$ii]#--}"
+done
+
+# Find the path to `ec2-ssh`.
+readonly EC2_SSH=$(readlink --canonicalize "$(dirname "${BASH_SOURCE[0]}")/../bin/ec2-ssh")
+
+set -x
+rsync \
+  --verbose \
+  --archive \
+  --delete \
+  --rsh "${EC2_SSH} ${AWS_OPTS[*]} --" \
+  --rsync-path 'sudo rsync' \
+  --compress \
+  "$@"
