4c4e369834e2f49b39764ba5d6f2b22cce76245d
Joshua Spence <josh@freelancer.com>
2018-07-09 11:20:23 +1000

    Minor formatting change

--- lib/ec2-list.sh
+++ lib/ec2-list.sh
@@ -5,9 +5,8 @@ OPTIONS='environment:,product:,profile:,role:,stack:'
 source "$(dirname "$(readlink --canonicalize "${BASH_SOURCE[0]}")")/common.sh"
 
 # shellcheck disable=SC2016
-aws "${AWS_OPTS[@]}" \
-  --output table \
-  --color off \
+aws \
+  "${AWS_OPTS[@]}" --output table --color off \
   ec2 describe-instances \
   --filters "${FILTERS[@]}" \
   --query 'Reservations[*].Instances[*].{ID:InstanceId, "Public IP": PublicIpAddress, "Private IP": PrivateIpAddress, Name: Tags[?Key==`Name`] | [0].Value}' \
